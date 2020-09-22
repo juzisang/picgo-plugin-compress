b9e9e93ab6e555d681cec1918dd4047cbad95b4e
hss01248 <hss012489@gmail.com>
2020-09-22 17:55:42 +0800

    集成luban算法

--- src/compress/luban.ts
+++ src/compress/luban.ts
@@ -3,53 +3,45 @@ import mozjpeg from 'imagemin-mozjpeg'
 import optipng from 'imagemin-optipng'
 import { CompressOptions, ImgInfo } from '../utils/interfaces'
 import { getImageBuffer } from '../utils/getImage'
-
-const pngToJpeg = require('png-to-jpeg');
-
-const Png = require("png-js");
-const Jpeg = require("jpeg-js");
 var images = require("images");
 
-export function lubanCompress({ ctx, info }: CompressOptions): Promise<ImgInfo> {
-  function getSample(info: ImgInfo) {
-    return ['2,2']
-  }
-  /*function getWidth(width: Number) {
 
-    if(width < 1080){
-      return width
-    }
-    if(width< 2160){
-      return width >> 1
-    }
-    if(width< 4320){
-      return width/4
-    }
-    return width/6
+
+export function lubanCompress({ ctx, info }: CompressOptions): Promise<ImgInfo> {
+  /*function getSample(info: ImgInfo) {
+    return ['1x1']
   }*/
+
+
+function  computeInSampleSize(srcWidth :number, srcHeight:number) {
+    srcWidth = srcWidth % 2 == 1 ? srcWidth + 1 : srcWidth;
+    srcHeight = srcHeight % 2 == 1 ? srcHeight + 1 : srcHeight;
+
+    var longSide = Math.max(srcWidth, srcHeight);
+    var shortSide = Math.min(srcWidth, srcHeight);
+
+    var scale = ( shortSide / longSide);
+    if (scale <= 1 && scale > 0.5625) {
+      if (longSide < 1664) {
+        return 1;
+      } else if (longSide < 4990) {
+        return 2;
+      } else if (longSide > 4990 && longSide < 10240) {
+        return 4;
+      } else {
+        return longSide / 1280 == 0 ? 1 : longSide / 1280;
+      }
+    } else if (scale <= 0.5625 && scale > 0.5) {
+      return longSide / 1280 == 0 ? 1 : longSide / 1280;
+    } else {
+      return  Math.ceil(longSide / (1280.0 / scale));
+    }
+  }
 
   function isJpg(buffer: Buffer) {
     return buffer[0] === 255 &&
       buffer[1] === 216 &&
       buffer[2] === 255
-  }
-  var sampleSize = getSample(info);
-  console.log("------xxxxx")
-
-  function getWidth(width: number | undefined) {
-    if(width == undefined){
-      return 700;
-    }
-    if(width < 1080){
-      return width
-    }
-    if(width< 2160){
-      return width /2
-    }
-    if(width< 4320){
-      return width /4
-    }
-    return width/6
   }
 
   return getImageBuffer(ctx, info.url)
@@ -58,37 +50,27 @@ export function lubanCompress({ ctx, info }: CompressOptions): Promise<ImgInfo> 
         ctx.log.warn('本身就是jpg,不用转换:'+info.url)
         return buffer
       }
-      ctx.log.warn('luban imagemin 格式转换为jpg:'+info.url)
-      return  images(buffer).encode("jpg", {operation:70})
+      ctx.log.warn('luban  格式转换为jpg:'+info.url)
+      return  images(buffer).encode("jpg", {operation:90})
     })
     .then((buffer)=>{
-      var image = images(buffer)
-      var width  = image.width
-      var conpressWidth = getWidth(width)
-      ctx.log.warn('luban imagemin width等比压缩:'+width+"-->"+conpressWidth)
-      return  images(buffer).resize(conpressWidth).encode("jpg", {operation:70})
+
+      var image2 = images(buffer)
+      //todo 关键在于获取图片本身的宽高
+      var samplesize = computeInSampleSize(image2.width(),image2.height())
+      var conpressWidth = image2.width()/samplesize
+      ctx.log.warn('luban  width等比压缩:'+image2.width()+"-->"+conpressWidth)
+      return  image2.resize(conpressWidth).encode("jpg", {operation:90})
     })
-    /*.then((buffer)=>{
-      if(info.url.indexOf('.png') >0){
-        ctx.log.warn('luban imagemin pngToJpeg:'+info.url)
-        let png = new Png(buffer);
-        return new Promise(resolve => png.decode(resolve))
-          .then(data => Jpeg.encode({data, width: png.width, height: png.height}, 70).data);
-        //return pngToJpeg({quality: 70})(buffer)
-      }else {
-        return buffer
-      }
-    })*/
-    /*.then((buffer) => {
-      ctx.log.warn('luban imagemin compress in progress')
-      ctx.log.info("isJpeg:"+isJpg(buffer))
-      images(buffer).save("/Users/hss/github/picgo-plugin-compressluban/images/"+"xxxx.jpg")
-      return imagemin.buffer(buffer, {
-        plugins: [mozjpeg({ quality: 70, progressive: true ,sample:sampleSize}),optipng({ optimizationLevel: 5 })],//, optipng({ optimizationLevel: 5 })
-      })
-    })*/
     .then((buffer) => {
-      ctx.log.warn('luban imagemin compress in success')
+      ctx.log.warn('luban  compress in progress')
+      //images(buffer).save("/Users/hss/github/picgo-plugin-compressluban/images/"+"xxxx.jpg")
+      return imagemin.buffer(buffer, {
+        plugins: [mozjpeg({ quality: 70}),optipng({ optimizationLevel: 5 })],//, optipng({ optimizationLevel: 5 })//, sample:sampleSize
+      })
+    })
+    .then((buffer) => {
+      ctx.log.warn('luban  compress in success')
       return {
         ...info,
         buffer,
